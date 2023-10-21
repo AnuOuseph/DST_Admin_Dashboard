8851120d2f0def4869209b6cee42febf023c2a1b
AnuOuseph <anuouseph04@gmail.com>
2023-10-22 02:07:14 +0530

    Add event completed

--- client/src/layouts/addEvents/index.js
+++ client/src/layouts/addEvents/index.js
@@ -53,8 +53,42 @@ import team3 from "assets/images/team-3.jpg";
 import team4 from "assets/images/team-4.jpg";
 import { Box, Card, TextField } from "@mui/material";
 import MDButton from "components/MDButton";
+import { useState } from "react";
+import axios from "axios";
 
 function AddEvents() {
+    const [error,setError] = useState(null)
+    const [message, setMessage] = useState(null)
+    const [formData, setFormData] = useState({
+        title: '',
+        date: '',
+        sport: '',
+        location: '',
+        description: '',
+        outcome: '',
+        team1: '',
+        team2: '',
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    console.log(formData)
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        try{
+            console.log("hahshagg")
+            const res = await axios.post("http://localhost:5000/api/admin/createEvent",formData)
+            const mess = res?.data?.message;
+            console.log("nbsbdhqb",res?.data)
+            setMessage(mess)
+            setFormData({ ...formData, [name]: '' })
+        }catch(err){
+            console.log(err)
+            setError(err)
+        }
+    };
   return (
     <DashboardLayout>
       <DashboardNavbar />
@@ -72,6 +106,9 @@ function AddEvents() {
                                     <TextField
                                         name="title"
                                         label="Title"
+                                        type="text"
+                                        value={formData.title}
+                                        onChange={handleChange}
                                         fullWidth
                                     />
                                 </Box>
@@ -79,6 +116,9 @@ function AddEvents() {
                                     <TextField
                                         name="date"
                                         label="Date"
+                                        type="date"
+                                        value={formData.date}
+                                        onChange={handleChange}
                                         fullWidth
                                     />
                                 </Box>
@@ -86,6 +126,9 @@ function AddEvents() {
                                     <TextField
                                         name="sport"
                                         label="Sport"
+                                        type="text"
+                                        value={formData.sport}
+                                        onChange={handleChange}
                                         fullWidth
                                     />
                                 </Box>
@@ -93,6 +136,9 @@ function AddEvents() {
                                     <TextField
                                         name="location"
                                         label="Location"
+                                        type="text"
+                                        value={formData.location}
+                                        onChange={handleChange}
                                         fullWidth
                                     />
                                 </Box>
@@ -100,6 +146,9 @@ function AddEvents() {
                                     <TextField
                                         name="description"
                                         label="Description"
+                                        type="text"
+                                        value={formData.description}
+                                        onChange={handleChange}
                                         fullWidth
                                     />
                                 </Box>
@@ -116,6 +165,9 @@ function AddEvents() {
                                 <TextField
                                     name="outcome"
                                     label="Outcome"
+                                    type="text"
+                                    value={formData.outcome}
+                                    onChange={handleChange}
                                     fullWidth
                                 />
                             </Box>
@@ -123,6 +175,9 @@ function AddEvents() {
                                 <TextField
                                     name="team1"
                                     label="Team1"
+                                    type="text"
+                                    value={formData.team1}
+                                    onChange={handleChange}
                                     fullWidth
                                 />
                             </Box>
@@ -130,12 +185,17 @@ function AddEvents() {
                                 <TextField
                                     name="team2"
                                     label="Team2"
+                                    type="text"
+                                    value={formData.team2}
+                                    onChange={handleChange}
                                     fullWidth
                                 />
                             </Box>
-                            <MDButton sx={{ margin: '20px' }} variant="gradient" color="dark" >
+                            <MDButton sx={{ margin: '20px' }} variant="gradient" color="dark" onClick={handleSubmit}>
                                 Create
                             </MDButton>
+                            <p style={{ fontSize: "12px", paddingX: "20px" }}>{message?message:null}</p>
+                            <p style={{ fontSize: "12px", paddingX: "20px", color: "red" }}>{error?error:null}</p>
                         </MDBox>
                         <MDBox pt={1} pb={2} px={2} lineHeight={1.25}>
                         </MDBox>
